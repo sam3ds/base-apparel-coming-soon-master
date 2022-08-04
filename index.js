32b74a11630c5a8bfb302c37b5358a672e6a7e0b
sam3ds <sam3ds@gmail.com>
2022-08-04 08:32:27 +0300

    working on mobile layout

--- index.js
+++ index.js
@@ -11,12 +11,21 @@ function ValidateEmail(inputText) {
 let emailBox = document.querySelector(".email");
 let emailInput = document.querySelector(".email input");
 let emailButton = document.querySelector(".email button");
+let exclamationCircle = document.querySelector(".fa-circle-exclamation")
+let errorMessage = document.querySelector(".error-message p")
 
 emailButton.addEventListener("click", () => {
     if (! ValidateEmail(emailInput)) {
         emailBox.style.borderColor = "hsl(0, 93%, 68%)";
         emailBox.style.borderWidth = "3px";
         emailInput.focus();
+        exclamationCircle.style.display = "block";
+        errorMessage.style.display = "block";
+    } else {
+        emailBox.style.borderColor = "hsl(0, 36%, 70%)";
+        emailBox.style.borderWidth = "1px";
+        exclamationCircle.style.display = "none";
+        errorMessage.style.display = "none";
     }
 
 })
